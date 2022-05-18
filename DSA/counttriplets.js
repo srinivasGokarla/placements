508435cf71cd224074369ef6a8eb91f7be9a4e21
srinivasGokarla <87422030+srinu217@users.noreply.github.com>
2022-05-18 11:45:31 +0530

    up

--- DSA/counttriplets.js
+++ DSA/counttriplets.js
@@ -25,24 +25,3 @@ function countTriplets(n,k,arr){
         return ans;
     }
  
-if (process.env.USERNAME === "srini") {
-    runProgram(`4 2
-    -3 -2 -1 0`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
