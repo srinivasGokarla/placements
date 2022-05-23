39de284845bf113a9e965a2e39d34080f8586c26
srinivasGokarla <87422030+srinu217@users.noreply.github.com>
2022-05-23 09:29:14 +0530

    dsa

---
+++ DSA/innerobjects.js
@@ -0,0 +1,30 @@
+
+var obj = {
+    a: {
+        b: {
+            c: 12,
+            j: false
+        },
+        k: null
+    }
+};
+
+const findPath = (object, path) => {
+    const paths = path.split('.');
+  let innerObj = {...obj};
+  for (let i = 0; i < paths.length; i++) {
+    innerObj = innerObj && innerObj[paths[i]] || null;
+  }
+  return innerObj;
+};
+
+
+console.log(findPath(obj, 'a.b.c')); // 12
+console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+console.log(findPath(obj, 'a.b.d')); // undefined
+console.log(findPath(obj, 'a.c')); // undefined
+console.log(findPath(obj, 'a.b.c.d')); // undefined
+console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+console.log(findPath(obj, 'a.b.j')); //false
+console.log(findPath(obj, 'a.b.j.k')); //undefined
+console.log(findPath(obj, 'a.k')); //null
