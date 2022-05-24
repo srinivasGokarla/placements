832486fc79b024432a77985e9b2a768e6d5b6fee
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2022-05-24 09:53:27 +0530

    Update anagramdetecter.js

--- DSA/anagramdetecter.js
+++ DSA/anagramdetecter.js
@@ -36,24 +36,3 @@ function AnagramDetective(res1,res2){
     return "False";
 }
 
-if (process.env.USERNAME === "srini") {
-    runProgram(`anagram
-    nag a ram`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
