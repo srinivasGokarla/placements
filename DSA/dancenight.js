eebc73cead89c6e7e7d3faa94ae3ff37de399cbe
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2022-06-04 21:07:53 +0530

    Changes

--- DSA/dancenight.js
+++ DSA/dancenight.js
@@ -33,22 +33,5 @@ function runProgram(input) {
       4 5
       2 5 6 8
       3 8 5 1 7`);
-    } else {
-      process.stdin.resume();
-      process.stdin.setEncoding("ascii");
-      let read = "";
-      process.stdin.on("data", function (input) {
-        read += input;
-      });
-      process.stdin.on("end", function () {
-        read = read.replace(/\n$/, "");
-        read = read.replace(/\n$/, "");
-        runProgram(read);
-      });
-      process.on("SIGINT", function () {
-        read = read.replace(/\n$/, "");
-        runProgram(read);
-        process.exit(0);
-      });
     }
     
