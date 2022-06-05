6b4ca0caf2b4810fe1140b7b50eb3889e2594f8c
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2022-06-05 10:09:06 +0530

    Change

--- DSA/binaryquivalent.js
+++ DSA/binaryquivalent.js
@@ -23,21 +23,4 @@ return  binaryEquivalent(N,str)
     runProgram(`2
     15
     128`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
   }
