68f694cc628ff6e1ce4210e85f11ca65a1f3e13b
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2022-06-07 19:22:35 +0530

    One

--- DSA/fizz.js
+++ DSA/fizz.js
@@ -27,21 +27,4 @@ if (process.env.USERNAME === "srini") {
   runProgram(`2
   1
   3`);
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-    read += input;
-  });
-  process.stdin.on("end", function () {
-    read = read.replace(/\n$/, "");
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-  });
-  process.on("SIGINT", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-    process.exit(0);
-  });
 }
